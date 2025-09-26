4ea38944b0ddc91929fcaaf360d17d2106931388
Asbjørn Rysgaard Eriksen <mail@bliztle.com>
2025-09-26 20:26:54 +0200

    removed unused wg lines

--- modules/wireguard.nix
+++ modules/wireguard.nix
@@ -22,20 +22,13 @@ in
       # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
       # For this to work you have to set the dnsserver IP of your router (or dnsserver of choice) in your clients
       postSetup = ''
-        # ${iptables} -t nat -A POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
         ${iptables} -t nat -A POSTROUTING -s 10.100.0.0/24 -o end0 -j MASQUERADE
-        # ${iptables} -A FORWARD -i wg0 -o eth0 -j ACCEPT
         ${iptables} -A FORWARD -i wg0 -o end0 -j ACCEPT
-        # ${iptables} -A FORWARD -i wg0 -j ACCEPT
-        # ${iptables} -A FORWARD -i eth0 -o wg0 -m state --state RELATED,ESTABLISHED -j ACCEPT
         ${iptables} -A FORWARD -i end0 -o wg0 -m state --state RELATED,ESTABLISHED -j ACCEPT
       '';
       
       # This undoes the above command
       postShutdown = ''
-        # ${iptables} -t nat -D POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
-        # ${iptables} -D FORWARD -i wg0 -o eth0 -j ACCEPT
-        # ${iptables} -D FORWARD -i eth0 -o wg0 -m state --state RELATED,ESTABLISHED -j ACCEPT
         ${iptables} -t nat -D POSTROUTING -s 10.100.0.0/24 -o end0 -j MASQUERADE
         ${iptables} -D FORWARD -i wg0 -o end0 -j ACCEPT
         ${iptables} -D FORWARD -i end0 -o wg0 -m state --state RELATED,ESTABLISHED -j ACCEPT
